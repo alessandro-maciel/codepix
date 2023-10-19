0871ec67a19838e2be68fe08295eed3b03e6463a
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-10-19 11:45:34 -0300

    refactor: grpc server configuration

--- application/grpc/server.go
+++ application/grpc/server.go
@@ -17,23 +17,23 @@ func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
 	reflection.Register(grpcServer)
 
-	pixRepository := repository.PixKeyRepositoryDb{}
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
 	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
-	PixGrpcService := NewPixGrpcService(pixUseCase)
+	pixGrpcService := NewPixGrpcService(pixUseCase)
 
-	pb.RegisterPixServiceServer(grpcServer, PixGrpcService)
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
 
 	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
-		log.Fatal("Cannot start grpc server", err)
+		log.Fatal("cannot start grpc server", err)
 	}
 
 	log.Printf("gRPC server has been started on port %d", port)
 	err = grpcServer.Serve(listener)
 
 	if err != nil {
-		log.Fatal("Cannot start grpc server", err)
+		log.Fatal("cannot start grpc server", err)
 	}
 }
